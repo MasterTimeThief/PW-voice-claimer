b6e983b91726c84ca0846a0aa8eb9bc97257838a
mastertimethief <mastertimethief@gmail.com>
2024-11-01 19:51:28 -0400

    Updated to AA3

--- src/app/character-list.ts
+++ src/app/character-list.ts
@@ -1,33 +1,41 @@
 import { Character } from "./character";
 
 export const CHARACTERS: Character[] = [
-    { id: 1, margin: 214, name: "Phoenix Wright",       file: "phoenix.gif" },
+    // MAIN CAST
+    //{ id: 1, margin: 214, name: "Phoenix Wright",       file: "phoenix.gif" },
+    { id: 1, margin: 214, name: "Mia Fey",              file: "aa3/y-mia.gif" },
     { id: 2, margin: 115, name: "Maya Fey",             file: "maya.gif" },
     { id: 3, margin: 115, name: "Pearl Fey",            file: "pearl.gif" },
     { id: 4, margin: 306, name: "The Judge",            file: "judge.gif" },
-    { id: 5, margin: 115, name: "Franziska Von Karma",  file: "franziska.gif" },
-    { id: 6, margin: 115, name: "Dick Gumshoe",         file: "gumshoe.gif" },
-    { id: 7, margin: 115, name: "Miles Edgeworth",      file: "edgeworth.gif" },
-    // Case 2
-    //{ id: 8, margin: 115, name: "Turner Grey",          file: "grey.gif" },
-    //{ id: 9, margin: 115, name: "Morgan Fey",           file: "morgan.gif" },
-    //{ id: 10, margin: 115, name: "Ini Miney",           file: "ini.gif" },
-    //{ id: 11, margin: 115, name: "Lotta Hart",          file: "lotta.gif" },
-    //{ id: 12, margin: 115, name: "Director Hotti",      file: "hotti.gif" },
-    ////Case 3
-    ////{ id: 13, margin: 115, name: "Max Galactica",           file: "max.gif" },
-    //{ id: 13, margin: 115, name: "Billy Bob Johns",         file: "billy.gif" },
-    //{ id: 14, margin: 115, name: "Benjamin Woodman",        file: "ben.gif" },
-    //{ id: 15, margin: 115, name: "Ben + Trilo Quist",       file: "bentrilo.gif" },
-    //{ id: 16, margin: 115, name: "Lawrence \"Moe\" Curls",  file: "moe.gif" },
-    //{ id: 17, margin: 115, name: "Regina Berry",            file: "regina.gif" },
-    //{ id: 18, margin: 115, name: "Ken \"Acro\" Dingling",   file: "acro.gif" },
-    //Case 4
-    { id: 8, margin: 115, name: "Matt Engarde",    file: "matt.gif" },
-    //{ id: 8, margin: 115, name: "Matt Engarde",    file: "matt-trueself.gif" },
-    { id: 9, margin: 115, name: "Adrian Andrews",  file: "adrian.gif" },
-    { id: 10, margin: 115, name: "Shelly de Killer", file: "killer.gif" },
-    //{ id: 11, margin: 115, name: "Wendy Oldbag", file: "wendys-bag.png" },
-    { id: 11, margin: 115, name: "Wendy Oldbag", file: "oldbag-alien.gif" },
-    { id: 12, margin: 115, name: "Will Powers", file: "will-suit.gif" },
+    { id: 5, margin: 115, name: "Dick Gumshoe",         file: "gumshoe.gif" },
+    { id: 6, margin: 214, name: "Winston Payne",        file: "payne.gif" },
+    //{ id: 6, margin: 214, name: "Godot",        file: "aa3/godot.gif" },
+    
+    //Case 1
+    { id: 7, margin: 115, name: "Marvin Grossberg",     file: "aa3/y-grossberg.gif" },
+    { id: 8, margin: 115, name: "Phoenix Wright",       file: "aa3/y-phoenix.gif" },
+    { id: 9, margin: 215, name: "Dahlia Hawthorne",     file: "aa3/dahlia.gif" },
+
+    /*
+    //Case 2
+    { id: 0, margin: 115, name: "Ron Delite",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Luke",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Andrews",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Larry",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Desiree",      file: "file.gif" },
+
+    //Case 3
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+     */
 ];
