825a8a29b386a9f792d621c1eb810e651da7aa38
mastertimethief <mastertimethief@gmail.com>
2024-11-09 01:06:32 -0500

    AA3 Case 2

--- src/app/character-list.ts
+++ src/app/character-list.ts
@@ -2,35 +2,40 @@ import { Character } from "./character";
 
 export const CHARACTERS: Character[] = [
     // MAIN CAST
-    //{ id: 1, margin: 214, name: "Phoenix Wright",       file: "phoenix.gif" },
-    { id: 1, margin: 214, name: "Mia Fey",              file: "aa3/y-mia.gif" },
+    { id: 1, margin: 215, name: "Phoenix Wright",       file: "phoenix.gif" },
+    //{ id: 1, margin: 215, name: "Mia Fey",              file: "aa3/y-mia.gif" },
     { id: 2, margin: 115, name: "Maya Fey",             file: "maya.gif" },
     { id: 3, margin: 115, name: "Pearl Fey",            file: "pearl.gif" },
     { id: 4, margin: 306, name: "The Judge",            file: "judge.gif" },
     { id: 5, margin: 115, name: "Dick Gumshoe",         file: "gumshoe.gif" },
-    { id: 6, margin: 214, name: "Winston Payne",        file: "payne.gif" },
-    //{ id: 6, margin: 214, name: "Godot",        file: "aa3/godot.gif" },
+    //{ id: 6, margin: 215, name: "Winston Payne",        file: "payne.gif" },
+    { id: 6, margin: 215, name: "Godot",        file: "aa3/godot.gif" },
     
     //Case 1
     { id: 7, margin: 115, name: "Marvin Grossberg",     file: "aa3/y-grossberg.gif" },
     { id: 8, margin: 115, name: "Phoenix Wright",       file: "aa3/y-phoenix.gif" },
     { id: 9, margin: 215, name: "Dahlia Hawthorne",     file: "aa3/dahlia.gif" },
 
-    /*
     //Case 2
-    { id: 0, margin: 115, name: "Ron Delite",      file: "file.gif" },
-    { id: 0, margin: 115, name: "Luke",      file: "file.gif" },
-    { id: 0, margin: 115, name: "Andrews",      file: "file.gif" },
-    { id: 0, margin: 115, name: "Larry",      file: "file.gif" },
-    { id: 0, margin: 115, name: "Desiree",      file: "file.gif" },
-
+    { id: 10, margin: 115, name: "Adrian Andrews",      file: "aa3/adrian.gif" },
+    { id: 11, margin: 115, name: "Ron DeLite",          file: "aa3/ron.gif" },
+    { id: 12, margin: 115, name: "Luke Atmey",          file: "aa3/atmey.gif" },
+    { id: 13, margin: 115, name: "Desiree DeLite",      file: "aa3/desiree.gif" },
+    { id: 14, margin: 115, name: "Larry Butz",          file: "aa3/larryKBE.gif" },
+    
+    /*
     //Case 3
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
-    { id: 0, margin: 115, name: "name",      file: "file.gif" },
+    
+    { id: 0, margin: 115, name: "Maggey Byrde",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Jean Armstrong",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Victor Kudo",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Viola Cadaverini",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Furio Tigre",      file: "file.gif" },
+    { id: 0, margin: 115, name: "Lisa Basil",      file: "file.gif" },
+    
+    
+    
+    
     { id: 0, margin: 115, name: "name",      file: "file.gif" },
     { id: 0, margin: 115, name: "name",      file: "file.gif" },
     { id: 0, margin: 115, name: "name",      file: "file.gif" },
