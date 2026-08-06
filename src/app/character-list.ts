28a60b0877ee3516fc0778d04ce83d05fdc65f10
mastertimethief <mastertimethief@gmail.com>
2026-08-05 23:25:14 -0400

    AAI2 Characters

--- src/app/character-list.ts
+++ src/app/character-list.ts
@@ -72,7 +72,7 @@ export const CHARACTERS: Character[] = [
     //Ace Attorney Investigations
 
     //Main characters
-    { id: 1, margin: 115, name: "Miles Edgeworth",     file: "aai/edge.png" },
+    /*{ id: 1, margin: 115, name: "Miles Edgeworth",     file: "aai/edge.png" },
     { id: 2, margin: 115, name: "Dick Gumshoe",        file: "aai/tile001.png" },
     { id: 3, margin: 115, name: "Kay Faraday",          file: "aai/tile010.png" },
     { id: 4, margin: 115, name: "Franziska von Karma",  file: "aai/tile009.png" },
@@ -106,9 +106,52 @@ export const CHARACTERS: Character[] = [
     { id: 21, margin: 115, name: "Manny Coachen",          file: "aai/tile032.png" },
     { id: 22, margin: 115, name: "Calisto Yew",          file: "aai/tile027.png" },
     { id: 23, margin: 115, name: "Tyrell Badd",          file: "aai/tile028.png" },
+    { id: 24, margin: 115, name: "Judgey McJudgeFace",          file: "aai/tile026.png" },
     
     //Turnabout Ablaze
-    { id: 24, margin: 115, name: "Colias Palaeno",          file: "aai/tile031.png" },
-    { id: 25, margin: 115, name: "Quercos Alba",          file: "aai/tile033.png" },
-    { id: 26, margin: 115, name: "Larry Butz",          file: "aai/tile034.png" },
+    { id: 25, margin: 115, name: "Colias Palaeno",          file: "aai/tile031.png" },
+    { id: 26, margin: 115, name: "Quercos Alba",          file: "aai/tile033.png" },
+    { id: 27, margin: 115, name: "Larry Butz",          file: "aai/tile034.png" },*/
+
+    { id: 1, margin: 115, name: "Miles Edgeworth",     file: "aai/edge.png" },
+    { id: 2, margin: 115, name: "Dick Gumshoe",        file: "aai2/tile001.png" },
+    { id: 3, margin: 115, name: "Kay Faraday",          file: "aai2/tile002.png" },
+    { id: 4, margin: 115, name: "Ema Skye",             file: "aai2/tile003.png" },
+    { id: 5, margin: 115, name: "Franziska von Karma",  file: "aai2/tile005.png" },
+    { id: 6, margin: 115, name: "Tyrell Badd",          file: "aai2/tile006.png" },
+    { id: 7, margin: 115, name: "Penny Nichols",          file: "aai2/tile007.png" },
+    { id: 8, margin: 115, name: "Shaun Fenn",          file: "aai2/tile008.png" },
+    { id: 9, margin: 115, name: "Hilda Hertz",          file: "aai2/tile011.png" },
+    { id: 10, margin: 115, name: "Excelsius Winner",          file: "aai2/tile012.png" },
+    
+    { id: 11, margin: 115, name: "Florence Needler",          file: "aai2/tile015.png" },
+    { id: 12, margin: 115, name: "Bastian Rook",          file: "aai2/tile018.png" },
+    { id: 13, margin: 115, name: "Rosie Ringer",          file: "aai2/tile020.png" },
+    { id: 14, margin: 115, name: "Shi-Long Lang",       file: "aai2/tile021.png" },
+    { id: 15, margin: 115, name: "Verity Gavèlle",          file: "aai2/tile022.png" },
+    { id: 16, margin: 115, name: "Eustace Winner",          file: "aai2/tile023.png" },
+    { id: 17, margin: 115, name: "Eddie Fender",          file: "aai2/tile025.png" },
+    { id: 18, margin: 115, name: "Manfred Von Karma",          file: "aai2/tile026.png" },
+    { id: 19, margin: 115, name: "Larry Butz",          file: "aai2/tile027.png" },
+    { id: 20, margin: 115, name: "Lotta Hart",          file: "aai2/tile028.png" },
+    
+    { id: 21, margin: 115, name: "Di-Jun Wang",          file: "aai2/tile030.png" },
+    { id: 22, margin: 115, name: "Bronco Knight",          file: "aai2/tile031.png" },
+    { id: 23, margin: 115, name: "Tabby Lloyd",          file: "aai2/tile032.png" },
+    { id: 24, margin: 115, name: "\"\"\"\"\"\"\"John Doe\"\"\"\"\"\"\"",          file: "aai2/tile033.png" },
+    { id: 25, margin: 115, name: "Bodhidharma Kanis",          file: "aai2/tile035.png" },
+    { id: 26, margin: 115, name: "Simeon Saint",          file: "aai2/tile036.png" },
+    { id: 27, margin: 115, name: "Rocco Carcerato",          file: "aai2/tile037.png" },
+    { id: 28, margin: 115, name: "Fifi Laguarde",          file: "aai2/tile038.png" },
+    { id: 29, margin: 115, name: "Frank Sahwit",          file: "aai2/tile040.png" },
+    { id: 30, margin: 115, name: "Regina Berry",          file: "aai2/tile041.png" },
+    
+    { id: 31, margin: 115, name: "Samson Tangaroa",          file: "aai2/tile043.png" },
+    { id: 32, margin: 115, name: "Just Bound",          file: "aai2/tile046.png" },
+    { id: 33, margin: 115, name: "Delicia Stone",          file: "aai2/tile048.png" },
+    { id: 34, margin: 115, name: "Carmelo Gusto",          file: "aai2/tile051.png" },
+    { id: 35, margin: 115, name: "Artie Frost",          file: "aai2/tile052.png" },
+    { id: 36, margin: 115, name: "Will Powers",          file: "aai2/tile053.png" },
+    { id: 37, margin: 115, name: "???",          file: "aai2/tile055.png" },
+    { id: 38, margin: 115, name: "Alf Aldown",          file: "aai2/tile056.png" },
 ];
